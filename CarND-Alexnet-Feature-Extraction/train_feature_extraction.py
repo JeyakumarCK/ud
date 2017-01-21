a6dd8eed4b827d5f52996aad0928073ab0ad3166
JeyakumarCK <JeyakumarCK@users.noreply.github.com>
2017-01-21 14:36:49 +0530

    upload from local pc

--- CarND-Alexnet-Feature-Extraction/train_feature_extraction.py
+++ CarND-Alexnet-Feature-Extraction/train_feature_extraction.py
@@ -1,14 +1,20 @@
+import time
 import pickle
 import tensorflow as tf
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
+from sklearn.utils import shuffle
 from alexnet import AlexNet
+from scipy.misc import imread
+import pandas as pd
+from tqdm import tqdm
+import numpy as np
 
 # TODO: Load traffic signs data.
 training_file = 'train.p'
 with open(training_file, mode='rb') as f:
     train = pickle.load(f)
 X_train, y_train = train['features'], train['labels']
-X_test, y_test = test['features'], test['labels']
+
 n_train = len(X_train)
 image_shape = X_train[0].shape
 nb_classes = 43
@@ -17,9 +23,11 @@ print("Number of training examples =", n_train)
 print("Image data shape =", image_shape)
 print("Number of classes =", nb_classes)
 
+sign_names = pd.read_csv('signnames.csv')
+
 # TODO: Split data into training and validation sets.
-from sklearn.model_selection import train_test_split
-X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=0.01, random_state=42)
+X_train, X_discard, y_train, y_discard = train_test_split(X_train, y_train, test_size=0.9, random_state=42)
 
 n_train = len(X_train)
 n_validation = len(X_validation)
@@ -28,13 +36,14 @@ print("Number of training examples =", n_train)
 print("Number of validation examples =", n_validation)
 
 # TODO: Define placeholders and resize operation.
-x_original = tf.placeholder(tf.float32, (None, 32, 32, 3))
-x = tf.image.resize_images(x, 227, 227)
+x = tf.placeholder(tf.float32, (None, 32, 32, 3))
+x_resized = tf.image.resize_images(x, 227, 227)
+
 y = tf.placeholder(tf.int32, (None))
 one_hot_y = tf.one_hot(y, 43)
 
 # TODO: pass placeholder as first argument to `AlexNet`.
-fc7 = AlexNet(x, feature_extract=True)
+fc7 = AlexNet(x_resized, feature_extract=True)
 # NOTE: `tf.stop_gradient` prevents the gradient from flowing backwards
 # past this point, keeping the weights before and up to `fc7` frozen.
 # This also makes training faster, less work to do!
@@ -51,7 +60,7 @@ probs = tf.nn.softmax(logits)
 # TODO: Define loss, training, accuracy operations.
 # HINT: Look back at your traffic signs project solution, you may
 # be able to reuse some the code.
-EPOCHS = 50
+EPOCHS = 5
 BATCH_SIZE = 128
 l_rate = 0.001
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, one_hot_y)
@@ -71,25 +80,8 @@ def evaluate(X_data, y_data):
         total_accuracy += (accuracy * len(batch_x))
     return total_accuracy / num_examples
 
-# TODO: Train and evaluate the feature extraction model.
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    num_examples = len(X_train)
-    
-    print("Training..........................")
-    print()
-    for i in range(EPOCHS):
-        X_train, y_train = shuffle(X_train, y_train)
-        for offset in range(0, num_examples, BATCH_SIZE):
-            end = offset + BATCH_SIZE
-            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
-            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
-            
-        validation_accuracy = evaluate(X_validation, y_validation)
-        print("EPOCH {} ...".format(i+1), "Validation Accuracy = {:.3f}".format(validation_accuracy))
-    print("Completed Training.................")
-       
 def predictTestImages():
+    print('Predicting the test images')
     # Read Images
     im1 = imread("construction.jpg").astype(np.float32)
     im1 = im1 - np.mean(im1)
@@ -110,4 +102,23 @@ def predictTestImages():
         print()
     
     print("Time: %.3f seconds" % (time.time() - t))
-predictTestImages()
+
+# TODO: Train and evaluate the feature extraction model.
+with tf.Session() as sess:
+    #sess.run(tf.global_variables_initializer())
+    sess.run(tf.initialize_all_variables())
+    num_examples = len(X_train)
+    
+    print("Training.........................")
+    print()
+    for i in tqdm(range(EPOCHS)):
+        X_train, y_train = shuffle(X_train, y_train)
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+            
+        validation_accuracy = evaluate(X_validation, y_validation)
+        print("EPOCH {} ...".format(i+1), "Validation Accuracy = {:.3f}".format(validation_accuracy))
+    print("Completed Training.................")
+    predictTestImages()   
